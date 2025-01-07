83016913e52d4b7b799793b35947cff0de17ac7e
Amruthachaluvadi <tdeveloper243@gmail.com>
2025-01-07 23:02:39 +0530

    New code var

---
+++ add.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Addition of 2 numbers"
+NUMBER1=$1
+NUMBER2=$2
+
+val=$NUMBER1+$NUMBER2
+
+echo "Sum of 2 numbers is : $val"
--- read-var.sh
+++ read-var.sh
@@ -3,3 +3,8 @@
 echo "Input First name :"
 read USERNAME
 echo "Username entered is : $USERNAME"
+
+echo "Enter Password::"
+read -s PASSWORD  # -s is used to hide the entered value
+
+
