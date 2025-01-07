24ca101ea32593ab994a86e3d64d7edfbaa66bde
Amruthachaluvadi <tdeveloper243@gmail.com>
2025-01-07 23:13:12 +0530

    New code var

--- add.sh
+++ add.sh
@@ -4,6 +4,8 @@ echo "Addition of 2 numbers"
 NUMBER1=$1
 NUMBER2=$2
 
-val=$NUMBER1+$NUMBER2
+TIMESTAMP=$(date)
+val=$(($NUMBER1+$NUMBER2))  #TO Execute dynamic cmds in shell we should use $ always
 
 echo "Sum of 2 numbers is : $val"
+echo "Current Time stamp : $TIMESTAMP"
