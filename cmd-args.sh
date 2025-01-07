3451f8e8ae188bd01560c0a1534fae15d7d70bb6
Amruthachaluvadi <tdeveloper243@gmail.com>
2025-01-07 18:26:33 +0530

    New code var

--- cmd-args.sh
+++ cmd-args.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if $? ne 2 
-then 
-  echo ("Usage of scrip is : sh cmd-arg.sh PERSON1 PERSON2" )
-PERSON1= $1
-PERSON2= $2
+#if $? ne 2 
+#then 
+ # echo ("Usage of script is : sh cmd-args.sh PERSON1 PERSON2")
+#fi
+
+PERSON1=$1
+PERSON2=$2
 
 
-echo "$PERSON1 : How are you $PERSON2?"
-echo  "$PERSON2: Doing great $PERSON1"
-echo "$PERSON1 : Where are u working $PERSON2?"
-echo "$PERSON2 : Working in dell, How are you $PERSON1"
+echo "$PERSON1 ::How are you $PERSON2?"
+echo  "$PERSON2:: Doing great $PERSON1"
+echo "$PERSON1 ::Where are u working $PERSON2?"
+echo "$PERSON2 :: Working in dell, How are you $PERSON1"
---
+++ read-var.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Input First name :"
+read USERNAME
+echo "Username entered is : $USERNAME"
