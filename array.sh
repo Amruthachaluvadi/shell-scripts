833c8d5194a703dbb06023c3226cedf423df0feb
Amruthachaluvadi <tdeveloper243@gmail.com>
2025-01-07 23:16:47 +0530

    New code var

--- array.sh
+++ array.sh
@@ -5,6 +5,6 @@ MOVIES=("pushpa" "rrr" "devara")
 
 echo "First Movie name:: ${MOVIES[0]}
 echo "Second Movie name:: ${MOVIES[1]}
-echo "ThirdMovie name:: ${MOVIES[1]}
+echo "Third Movie name:: ${MOVIES[2]}
 
 echo " Printt all movie names:: ${MOVIES[@]}
