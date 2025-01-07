6084e33fb5238d6739a20487c43fa83877de94e9
Amruthachaluvadi <tdeveloper243@gmail.com>
2025-01-07 11:08:09 +0530

    New code var

---
+++ variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1="Rahim"
+PERSON2="Sundeep"
+
+
+echo "$PERSON1 : How are you $PERSON2?"
+echo  "$PERSON2: Doing great $PERSON1"
+echo "$PERSON1 : Where are u working $PERSON2?"
+echo "$PERSON2 : Working in dell, How are you $PERSON1"
